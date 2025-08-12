71ad37388e534b412f020704eeb1720a61c125a7
Preslav977 <p.cvetanow@gmail.com>
2025-08-12 09:35:45 +0300

    Imported the context in the App, and created provider with Outlet so the states in App will be accesiable in all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import { UserSignUpContext } from "./context/UserSignUpContext";
 
-function App() {
-  return <></>;
+export function App() {
+  const [userSignUp, setUserSignUp] = useState({
+    username: "",
+    display_name: "",
+    bio: "",
+    website: "",
+    github: "",
+    profile_picture: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  return (
+    <>
+      <UserSignUpContext.Provider
+        value={[userSignUp, setUserSignUp]}
+      ></UserSignUpContext.Provider>
+    </>
+  );
 }
-
-export default App;
