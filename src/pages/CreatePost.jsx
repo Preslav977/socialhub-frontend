ecbdb2bb6bb6e8506e9bbe9334a34b94fecf2698
Preslav977 <p.cvetanow@gmail.com>
2025-08-20 13:14:39 +0300

    Had a problem with uploading a image turns out that my project was paused in Supabase, implemented uploading a post with text and with text and image

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,19 +1,120 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { localhostURL } from "../../utility/localhostURL";
+import { UserLogInContext } from "../context/UserLogInContext";
 import styles from "./CreatePost.module.css";
 
 export function CreatePost() {
   const [postLetterLength, setPostLetterLength] = useState("0");
 
+  const [userLogIn, setUserLogIn] = useContext(UserLogInContext);
+
+  const [checkIfImgIsUploaded, setCheckIfImageIsUploaded] = useState(false);
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+
+  const onSubmitPostText = async (data) => {
+    const { content, tag } = data;
+
+    try {
+      const response = await fetch(`${localhostURL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          content: content,
+          tag: tag,
+          authorId: userLogIn.id,
+        }),
+      });
+
+      const result = await response.json();
+
+      console.log(result);
+
+      reset();
+    } catch (error) {
+      throw error;
+    } finally {
+      setCheckIfImageIsUploaded(false);
+    }
+  };
+
+  const onSubmitPostTextAndImage = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    formData.append("authorId", Number(userLogIn.id));
+
+    try {
+      const response = await fetch(`${localhostURL}/posts/with-image`, {
+        method: "POST",
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        body: formData,
+      });
+
+      const result = await response.json();
+
+      reset();
+    } catch (error) {
+      throw error;
+    } finally {
+      setCheckIfImageIsUploaded(false);
+    }
+  };
+
+  const onChange = (e) => {
+    setPostLetterLength(e.target.value.length);
+  };
+
   return (
-    <form className={styles.formCreatePostContainer}>
+    <form
+      encType="multipart/formdata"
+      className={styles.formCreatePostContainer}
+      onSubmit={
+        checkIfImgIsUploaded
+          ? (e) => handleSubmit(onSubmitPostTextAndImage(e))
+          : handleSubmit(onSubmitPostText)
+      }
+    >
+      <label htmlFor="content"></label>
       <textarea
         className={styles.formTextarea}
-        onChange={(e) => setPostLetterLength(e.target.value.length)}
+        onChange={onChange}
         placeholder="Share whats happening..."
-        name="text"
-        id="text"
+        name="content"
+        id="content"
+        aria-label="content"
         rows={3}
+        {...register("content", {
+          required: true,
+          minLength: 1,
+          maxLength: 2000,
+          onChange,
+        })}
+        aria-invalid={errors.content ? "true" : "false"}
       ></textarea>
+
+      {errors.content?.type === "required" && (
+        <span role="alert">Content is required</span>
+      )}
+
+      {errors.content?.type === "minLength" ||
+        (errors.content?.type === "maxLength" && (
+          <span role="alert">
+            Content must be between 1 and 2000 characters
+          </span>
+        ))}
 
       <div className={styles.formFlexedControlsContainer}>
         <div className={styles.formControls}>
@@ -22,7 +123,13 @@ export function CreatePost() {
             src="./upload-image.svg"
             alt="uploading a image"
           />
-          <input type="file" name="file" id="file" />
+          <label htmlFor="file"></label>
+          <input
+            onClick={() => setCheckIfImageIsUploaded(true)}
+            type="file"
+            name="file"
+            id="file"
+          />
 
           <img
             className={styles.formControlsImg}
@@ -40,13 +147,28 @@ export function CreatePost() {
       </div>
 
       <div className={styles.formTagFlexedContainer}>
-        <label htmlFor="tags">Tags:</label>
+        <label htmlFor="tag">Tags:</label>
         <input
           className={styles.formInputTag}
           type="text"
-          name="tags"
-          id="tags"
+          name="tag"
+          id="tag"
+          aria-label="tag"
+          {...register("tag", {
+            required: true,
+            minLength: 1,
+            maxLength: 30,
+          })}
         />
+
+        {errors.tag?.type === "required" && (
+          <span role="alert">Tags are required</span>
+        )}
+
+        {errors.tag?.type === "minLength" ||
+          (errors.tag?.type === "maxLength" && (
+            <span role="alert">Tag must be between 1 and 2000 characters</span>
+          ))}
       </div>
     </form>
   );
