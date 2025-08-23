d0a23eb6b9ee5217b47e0db6553dcaefd6e42d0b
Preslav977 <p.cvetanow@gmail.com>
2025-08-23 09:21:20 +0300

    Created test to navigate to users and render its content

--- src/tests/MainGridInterface.test.jsx
+++ src/tests/MainGridInterface.test.jsx
@@ -803,7 +803,7 @@ describe("should render MainGridInterface", () => {
     expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
   });
 
-  it.only("should login navigate to settings and logout", async () => {
+  it("should login navigate to settings and logout", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/login", "/", "/settings"],
       initialIndex: 0,
@@ -906,5 +906,104 @@ describe("should render MainGridInterface", () => {
     expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
 
     await user.click(screen.queryByText("Logout"));
+  });
+
+  it.only("should login and navigate to users and render its content", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/", "/users"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const spiedFetch = vi.spyOn(global, "fetch");
+
+    spiedFetch
+      .mockResolvedValueOnce(Response.json("token"))
+      .mockResolvedValueOnce(
+        Response.json({
+          id: 1,
+          username: "preslaw",
+          display_name: "preslaw",
+          bio: "",
+          website: "",
+          github: "",
+          password: "12345678B",
+          confirm_password: "12345678B",
+          profile_picture: "",
+          followedBy: [],
+          following: [],
+          followersNumber: 0,
+          followingNumber: 0,
+          posts: 0,
+        }),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 2,
+            username: "preslaw",
+            display_name: "preslaw",
+          },
+          {
+            id: 3,
+            username: "preslaw1",
+            display_name: "preslaw1",
+          },
+          {
+            id: 4,
+            username: "preslaw2",
+            display_name: "preslaw2",
+          },
+        ]),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 5,
+            username: "test",
+            display_name: "test",
+          },
+          {
+            id: 6,
+            username: "test1",
+            display_name: "test1",
+          },
+          {
+            id: 7,
+            username: "test2",
+            display_name: "test2",
+          },
+        ]),
+      );
+
+    await user.type(screen.queryByLabelText("username"), "preslaw");
+
+    expect(screen.queryByLabelText("username").value).toEqual("preslaw");
+
+    await user.type(screen.queryByLabelText("password"), "12345678B");
+
+    expect(screen.queryByLabelText("password").value).toEqual("12345678B");
+
+    await user.click(screen.queryByRole("button", { name: "Login" }));
+
+    expect(screen.queryByAltText("loading spinner")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(
+      () => screen.queryByAltText("loading spinner"),
+      {
+        timeout: 1000,
+      },
+    );
+
+    await user.click(screen.queryByText("Users"));
+
+    expect(screen.queryAllByPlaceholderText("Enter a username"));
+
+    expect(screen.queryByText("No results").textContent).toMatch(/no results/i);
   });
 });
