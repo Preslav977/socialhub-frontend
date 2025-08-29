760a3372500bcb68baafa8e1673ba0bbe8289433
Preslav977 <p.cvetanow@gmail.com>
2025-08-29 13:20:43 +0300

    Created test to render message if he doesnt have created posts in his profile

--- src/tests/MainGridInterface.test.jsx
+++ src/tests/MainGridInterface.test.jsx
@@ -2188,7 +2188,7 @@ describe("should render MainGridInterface", () => {
     expect(screen.queryAllByText("0")[1].textContent).toEqual("0");
   });
 
-  it.only("should login navigate to user profile and like a post", async () => {
+  it("should login navigate to user profile and like a post", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/login", "/", "/profile/1"],
       initialIndex: 0,
@@ -2399,5 +2399,110 @@ describe("should render MainGridInterface", () => {
     await user.click(screen.queryByTestId("articleLike"));
 
     expect(screen.queryByText("1").textContent).toEqual("1");
+  });
+
+  it.only("should login and render message that the user doesn't have created by him posts", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/", "/profile/1"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const spiedFetch = vi.spyOn(global, "fetch");
+
+    spiedFetch
+      .mockResolvedValueOnce(Response.json("token"))
+      .mockResolvedValueOnce(
+        Response.json({
+          id: 1,
+          username: "preslaw",
+          display_name: "preslaw",
+          bio: "",
+          website: "",
+          github: "",
+          password: "12345678B",
+          confirm_password: "12345678B",
+          profile_picture: "./user-default-pfp.jpg",
+          followedBy: [],
+          following: [],
+          createdPostsByUsers: [],
+          followersNumber: 0,
+          followingNumber: 0,
+          posts: 0,
+        }),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json({ message: "No created posts by author!" }),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 2,
+            username: "preslaw",
+            display_name: "preslaw",
+          },
+          {
+            id: 3,
+            username: "preslaw1",
+            display_name: "preslaw1",
+          },
+          {
+            id: 4,
+            username: "preslaw2",
+            display_name: "preslaw2",
+          },
+        ]),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 5,
+            username: "test",
+            display_name: "test",
+          },
+          {
+            id: 6,
+            username: "test1",
+            display_name: "test1",
+          },
+          {
+            id: 7,
+            username: "test2",
+            display_name: "test2",
+          },
+        ]),
+      );
+
+    await user.type(screen.queryByLabelText("username"), "preslaw");
+
+    expect(screen.queryByLabelText("username").value).toEqual("preslaw");
+
+    await user.type(screen.queryByLabelText("password"), "12345678B");
+
+    expect(screen.queryByLabelText("password").value).toEqual("12345678B");
+
+    await user.click(screen.queryByRole("button", { name: "Login" }));
+
+    expect(screen.queryByAltText("loading spinner")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByAltText("loading spinner"),
+    );
+
+    expect(screen.queryByText("Profile"));
+
+    expect(screen.queryByText("Loading..."));
+
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+    expect(
+      screen.queryByText("No created posts by author!").textContent,
+    ).toMatch(/no created posts by author!/i);
   });
 });
