f30d115b95e01ab2bbd223ab9a8017f988172079
Preslav977 <p.cvetanow@gmail.com>
2025-08-29 12:56:21 +0300

    Created test for showing message if the user has no liked posts

--- src/tests/MainGridInterface.test.jsx
+++ src/tests/MainGridInterface.test.jsx
@@ -1702,7 +1702,7 @@ describe("should render MainGridInterface", () => {
     ).toMatch(/no posts has been created!/i);
   });
 
-  it.only("should login and navigate to likes and render liked posts", async () => {
+  it("should login and navigate to likes and render liked posts", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/login", "/", "/likes"],
       initialIndex: 0,
@@ -1870,5 +1870,110 @@ describe("should render MainGridInterface", () => {
     expect(screen.queryAllByText("0")[0].textContent).toEqual("0");
 
     expect(screen.queryByText("1").textContent).toEqual("1");
+  });
+
+  it.only("should login and render message that the user doesn't have liked posts", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/", "/likes"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const spiedFetch = vi.spyOn(global, "fetch");
+
+    spiedFetch
+      .mockResolvedValueOnce(Response.json("token"))
+      .mockResolvedValueOnce(
+        Response.json({
+          id: 1,
+          username: "preslaw",
+          display_name: "preslaw",
+          bio: "",
+          website: "",
+          github: "",
+          password: "12345678B",
+          confirm_password: "12345678B",
+          profile_picture: "./user-default-pfp.jpg",
+          followedBy: [],
+          following: [],
+          createdPostsByUsers: [],
+          followersNumber: 0,
+          followingNumber: 0,
+          posts: 0,
+        }),
+      )
+
+      .mockResolvedValueOnce(Response.json({ message: "No liked posts!" }))
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 2,
+            username: "preslaw",
+            display_name: "preslaw",
+          },
+          {
+            id: 3,
+            username: "preslaw1",
+            display_name: "preslaw1",
+          },
+          {
+            id: 4,
+            username: "preslaw2",
+            display_name: "preslaw2",
+          },
+        ]),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 5,
+            username: "test",
+            display_name: "test",
+          },
+          {
+            id: 6,
+            username: "test1",
+            display_name: "test1",
+          },
+          {
+            id: 7,
+            username: "test2",
+            display_name: "test2",
+          },
+        ]),
+      );
+
+    await user.type(screen.queryByLabelText("username"), "preslaw");
+
+    expect(screen.queryByLabelText("username").value).toEqual("preslaw");
+
+    await user.type(screen.queryByLabelText("password"), "12345678B");
+
+    expect(screen.queryByLabelText("password").value).toEqual("12345678B");
+
+    await user.click(screen.queryByRole("button", { name: "Login" }));
+
+    expect(screen.queryByAltText("loading spinner")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByAltText("loading spinner"),
+    );
+
+    expect(screen.queryByText("Loading..."));
+
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+    expect(screen.queryByText("Recent").textContent).toMatch(/recent/i);
+
+    expect(screen.queryByText("Following").textContent).toMatch(/following/i);
+
+    expect(screen.queryByText("No liked posts!").textContent).toMatch(
+      /no liked posts!/i,
+    );
   });
 });
