67a301c619c228cf714fb26f7e34b8e7fa16031c
Preslav977 <p.cvetanow@gmail.com>
2025-08-29 10:49:25 +0300

    Created test for rendering a post in the home page

--- src/tests/MainGridInterface.test.jsx
+++ src/tests/MainGridInterface.test.jsx
@@ -1134,7 +1134,7 @@ describe("should render MainGridInterface", () => {
     expect(screen.queryAllByText("Posts")[1].textContent).toMatch(/posts/i);
   });
 
-  it.only("should login navigate to profile and edit it", async () => {
+  it("should login navigate to profile and edit it", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/login", "/", "/profile/1"],
       initialIndex: 0,
@@ -1291,5 +1291,150 @@ describe("should render MainGridInterface", () => {
     await user.upload(bgImageInput, file);
 
     await user.click(screen.queryByRole("button", { name: "Save Changes" }));
+  });
+
+  it.only("should login and navigate to home and render posts", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const spiedFetch = vi.spyOn(global, "fetch");
+
+    spiedFetch
+      .mockResolvedValueOnce(Response.json("token"))
+      .mockResolvedValueOnce(
+        Response.json({
+          id: 1,
+          username: "preslaw",
+          display_name: "preslaw",
+          bio: "",
+          website: "",
+          github: "",
+          password: "12345678B",
+          confirm_password: "12345678B",
+          profile_picture: "./user-default-pfp.jpg",
+          followedBy: [],
+          following: [],
+          createdPostsByUsers: [],
+          followersNumber: 0,
+          followingNumber: 0,
+          posts: 0,
+        }),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 1,
+            content: "post on home",
+            imageURL: null,
+            tag: "post",
+            likes: 0,
+            comments: 0,
+            createdAt: new Date(),
+            authorId: 1,
+            postLikedByUsers: [],
+            author: {
+              id: 1,
+              username: "author",
+              display_name: "user",
+              bio: "",
+              website: "",
+              github: "",
+              password: "12345678B",
+              confirm_password: "12345678B",
+              profile_picture: "",
+              role: "USER",
+              followersNumber: 0,
+              followingNumber: 0,
+              posts: 1,
+              createdAt: new Date(),
+            },
+          },
+        ]),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 2,
+            username: "preslaw",
+            display_name: "preslaw",
+          },
+          {
+            id: 3,
+            username: "preslaw1",
+            display_name: "preslaw1",
+          },
+          {
+            id: 4,
+            username: "preslaw2",
+            display_name: "preslaw2",
+          },
+        ]),
+      )
+
+      .mockResolvedValueOnce(
+        Response.json([
+          {
+            id: 5,
+            username: "test",
+            display_name: "test",
+          },
+          {
+            id: 6,
+            username: "test1",
+            display_name: "test1",
+          },
+          {
+            id: 7,
+            username: "test2",
+            display_name: "test2",
+          },
+        ]),
+      );
+
+    await user.type(screen.queryByLabelText("username"), "preslaw");
+
+    expect(screen.queryByLabelText("username").value).toEqual("preslaw");
+
+    await user.type(screen.queryByLabelText("password"), "12345678B");
+
+    expect(screen.queryByLabelText("password").value).toEqual("12345678B");
+
+    await user.click(screen.queryByRole("button", { name: "Login" }));
+
+    expect(screen.queryByAltText("loading spinner")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByAltText("loading spinner"),
+    );
+
+    expect(screen.queryByText("Loading..."));
+
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+    screen.debug();
+
+    expect(screen.queryByText("author").textContent).toMatch(/author/i);
+
+    expect(screen.queryByText("less than a minute ago").textContent).toMatch(
+      /less than a minute ago/i,
+    );
+
+    expect(screen.queryByText("post on home").textContent).toMatch(
+      /post on home/i,
+    );
+
+    expect(screen.queryByText("author").textContent).toMatch(/author/i);
+
+    expect(screen.queryAllByText("0")[0].textContent).toEqual("0");
+
+    expect(screen.queryAllByText("0")[1].textContent).toEqual("0");
   });
 });
