8b3c68bc401fb0164e3b3b026d946cbbcdc52238
Preslav977 <p.cvetanow@gmail.com>
2025-09-01 11:20:03 +0300

    Currently trying to update the user who liked the other user comment

--- src/components/PostDetailsPropsComponent/PostDetailsPropsComponent.jsx
+++ src/components/PostDetailsPropsComponent/PostDetailsPropsComponent.jsx
@@ -1,59 +1,182 @@
 import { formatDistance } from "date-fns";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
 import { UserLogInContext } from "../../context/UserLogInContext";
 import styles from "./PostDetailsPropsComponent.module.css";
 
-export function PostDetails({ post }) {
+export function PostDetailsPropsComponent({
+  post,
+  onClickLikePost,
+  onClickLikeComment,
+  onSubmitComment,
+}) {
+  const { register, handleSubmit } = useForm();
+
   const [userLogIn, setUserLogIn] = useContext(UserLogInContext);
 
+  const [toggleReplyOnComment, setToggleReplyOnComment] = useState(false);
+
+  const onToggledReplyOnComment = () =>
+    setToggleReplyOnComment((toggleReplyOnComment) => !toggleReplyOnComment);
+
   return (
-    <article className={styles.articlePostContainer}>
-      <article className={styles.articlePostAuthor}>
-        <img
-          className={styles.articleAuthorImg}
-          src={post.author.profile_picture}
-          alt=""
-        />
-        <span id="articleAuthor">{post.author.username}</span>
-        <span>
-          {formatDistance(post.createdAt, new Date(), {
-            addSuffix: true,
-          })}
-        </span>
-      </article>
-      <p>{post.content}</p>
-
-      {post.imageURL ? (
-        <img className={styles.articlePostImg} src={post.imageURL} alt="" />
-      ) : (
-        ""
-      )}
-      <article className={styles.articlePostLikeAndComment}>
-        <div className={styles.articlePostLikesContainer}>
-          {!post.postLikedByUsers.some((user) => user.id === userLogIn.id) ? (
+    <>
+      <article className={styles.articlePostContainer}>
+        <article className={styles.articlePostAuthor}>
+          <div className={styles.articlePostAuthorAndDate}>
             <img
-              data-testid="articleLike"
-              id="articleLike"
-              className={styles.articleLike}
-              src="/likes.svg"
-              alt="like the post"
+              className={styles.articleAuthorImg}
+              src={post.author.profile_picture}
+              alt=""
             />
+
+            <span>{post.author.username}</span>
+            <span>
+              {formatDistance(post.createdAt, new Date(), {
+                addSuffix: true,
+              })}
+            </span>
+          </div>
+
+          {post.author.id === userLogIn.id ? (
+            <div>
+              <img
+                className={styles.articleDelete}
+                src="/trashcan.svg"
+                alt=""
+              />
+            </div>
           ) : (
-            <img
-              id="articleLike"
-              className={styles.articleLike}
-              src="/liked.png"
-              alt="dislike the post"
-            />
+            ""
           )}
+        </article>
+        <p>{post.content}</p>
 
-          <p>{post.likes}</p>
-        </div>
-        <div className={styles.articlePostCommentsContainer}>
-          <img className={styles.articleComment} src="/comment.svg" alt="" />
-          <p>{post.comments}</p>
-        </div>
+        {post.imageURL ? (
+          <img className={styles.articlePostImg} src={post.imageURL} alt="" />
+        ) : (
+          ""
+        )}
+        <article className={styles.articlePostLikeAndComment}>
+          <div className={styles.articlePostLikesContainer}>
+            {!post.postLikedByUsers.some((user) => user.id === userLogIn.id) ? (
+              <img
+                onClick={() => onClickLikePost(post)}
+                data-testid="articleLike"
+                className={styles.articleLike}
+                src="/likes.svg"
+                alt="like the post"
+              />
+            ) : (
+              <img
+                onClick={() => onClickLikePost(post)}
+                className={styles.articleLike}
+                src="/liked.png"
+                alt="dislike the post"
+              />
+            )}
+
+            <p>{post.likes}</p>
+          </div>
+          <div className={styles.articlePostCommentsContainer}>
+            <img className={styles.articleComment} src="/comment.svg" alt="" />
+            <p>{post.comments}</p>
+          </div>
+        </article>
       </article>
-    </article>
+      <form
+        onSubmit={handleSubmit(onSubmitComment)}
+        className={styles.createArticleComment}
+      >
+        <label htmlFor="text"></label>
+        <textarea
+          name="text"
+          id="text"
+          rows={3}
+          placeholder="Type a comment..."
+          {...register("text")}
+        ></textarea>
+        <button className={styles.createCommentBtn} type="submit">
+          Post
+        </button>
+      </form>
+      <>
+        <p>View comments {post.comments}</p>
+        {post.postCommentedByUsers.map((comment) => (
+          <div className={styles.articleCommentContainer}>
+            <div className={styles.articleCommentUser}>
+              <img
+                className={styles.articleUserImg}
+                src={comment.commentLeftByUser.profile_picture}
+                alt=""
+              />
+
+              <p>{comment.commentLeftByUser.username}</p>
+
+              <p>
+                {formatDistance(comment.createdAt, new Date(), {
+                  addSuffix: true,
+                })}
+              </p>
+            </div>
+
+            <p>{comment.text}</p>
+
+            <div className={styles.articlePostLikeAndComment}>
+              <div className={styles.articlePostLikesContainer}>
+                {!comment.commentLikedByUsers.some(
+                  (user) => user.id === userLogIn.id,
+                ) ? (
+                  <img
+                    onClick={() => onClickLikeComment(comment)}
+                    data-testid="articleLike"
+                    className={styles.articleLike}
+                    src="/likes.svg"
+                    alt="like the comment"
+                  />
+                ) : (
+                  <img
+                    onClick={() => onClickLikeComment(comment)}
+                    className={styles.articleLike}
+                    src="/liked.png"
+                    alt="dislike the comment"
+                  />
+                )}
+                <p>{comment.likes}</p>
+              </div>
+
+              <div
+                onClick={() => onToggledReplyOnComment()}
+                className={styles.articlePostCommentsContainer}
+              >
+                <img
+                  className={styles.articleComment}
+                  src="/comment-reply.svg"
+                  alt=""
+                />
+                <span>reply</span>
+              </div>
+            </div>
+            {toggleReplyOnComment ? (
+              <form className={styles.createReplyArticleComment}>
+                <label htmlFor="text"></label>
+                <textarea
+                  name="text"
+                  id="text"
+                  rows={3}
+                  placeholder="Add a reply..."
+                  {...register("text")}
+                ></textarea>
+                <button className={styles.createReplyCommentBtn} type="submit">
+                  Reply
+                </button>
+              </form>
+            ) : (
+              ""
+            )}
+          </div>
+        ))}
+      </>
+    </>
   );
 }
--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -1,7 +1,143 @@
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { localhostURL } from "../../../utility/localhostURL";
 import { useFetchPost } from "../../api/useFetchPost";
+import { PostDetailsPropsComponent } from "../../components/PostDetailsPropsComponent/PostDetailsPropsComponent";
 
 export function PostDetails() {
   const { post, setPost, loading, error } = useFetchPost();
 
   console.log(post);
+
+  const { id } = useParams();
+
+  const { reset } = useForm();
+
+  async function likeOrDislikePost(post) {
+    try {
+      const response = await fetch(`${localhostURL}/posts/${Number(id)}/like`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          id: post.id,
+        }),
+      });
+      const result = await response.json();
+
+      const likedOrDislikedPostObject = {
+        ...post.likedByUsers,
+        postLikedByUsers: result.postLikedByUsers,
+        likes: result.likes,
+      };
+
+      setPost(likedOrDislikedPostObject);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function likeOrDislikeComment(comment) {
+    // console.log(comment);
+
+    try {
+      const response = await fetch(
+        `${localhostURL}/posts/${id}/like/${comment.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("token"),
+          },
+          body: JSON.stringify({
+            id: comment.commentRelatedToPostId,
+            commentId: comment.id,
+          }),
+        },
+      );
+      const result = await response.json();
+
+      console.log(result);
+
+      const likedOrDislikedCommentObject = {
+        ...post,
+        // postCommentedByUsers:
+        //   ...post.postCommentedByUsers,
+        //   postCommentedByUsers: post.postCommentedByUsers.map((likedComment) => {
+        //     console.log(likedComment);
+        //     if (comment.id === likedComment.id) {
+        //       return;
+        //     } else {
+        //       return {
+        //         ...likedComment,
+        //         likes: result.likes,
+        //       };
+        //     }
+        //   }),
+      };
+
+      setPost(likedOrDislikedCommentObject);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function leavingAComment(data) {
+    const { text } = data;
+
+    try {
+      const response = await fetch(`${localhostURL}/posts/${post.id}/comment`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          id: post.id,
+          text,
+        }),
+      });
+
+      const result = await response.json();
+
+      console.log(result);
+
+      const commentPostObject = {
+        ...post,
+        postCommentedByUsers: result.postCommentedByUsers,
+        comments: result.comment,
+      };
+
+      setPost(commentPostObject);
+
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error...</p>;
+  }
+
+  return (
+    <>
+      {post ? (
+        <PostDetailsPropsComponent
+          post={post}
+          // onClickLikePost={likeOrDislikePost}
+          onClickLikeComment={likeOrDislikeComment}
+          onSubmitComment={leavingAComment}
+        />
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
