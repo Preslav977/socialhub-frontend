fb8f1e6c33863ce5b00fed080d91ecaf9b595d55
Preslav977 <p.cvetanow@gmail.com>
2025-09-14 10:57:32 +0300

    Need to figure out how to press Enter on input without the button

--- src/tests/Users.test.jsx
+++ src/tests/Users.test.jsx
@@ -5,7 +5,8 @@ import {
 } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
+import { localhostURL } from "../../utility/localhostURL";
 import { routes } from "../router/routes";
 
 describe("should render Users component", () => {
@@ -19,48 +20,14 @@ describe("should render Users component", () => {
 
     const user = userEvent.setup();
 
-    const spiedFetch = vi.spyOn(global, "fetch");
+    const response = await fetch(`${localhostURL}/login`, {
+      method: "POST",
+    });
 
-    spiedFetch
-      .mockResolvedValueOnce(Response.json("token"))
-      .mockResolvedValueOnce(
-        Response.json({
-          id: 1,
-          username: "preslaw",
-          display_name: "preslaw",
-          bio: "",
-          website: "",
-          github: "",
-          password: "12345678B",
-          confirm_password: "12345678B",
-          profile_picture: "",
-          followedBy: [],
-          following: [],
-          followersNumber: 0,
-          followingNumber: 0,
-          posts: 0,
-        }),
-      )
-
-      .mockResolvedValueOnce(
-        Response.json([
-          {
-            id: 2,
-            username: "preslaw",
-            display_name: "preslaw",
-          },
-          {
-            id: 3,
-            username: "preslaw1",
-            display_name: "preslaw1",
-          },
-          {
-            id: 4,
-            username: "preslaw2",
-            display_name: "preslaw2",
-          },
-        ]),
-      );
+    await expect(response.json()).resolves.toEqual({
+      username: "preslaw",
+      password: "12345678B",
+    });
 
     await user.type(screen.queryByLabelText("username"), "preslaw");
 
@@ -82,8 +49,58 @@ describe("should render Users component", () => {
 
     expect(screen.queryAllByPlaceholderText("Enter a username"));
 
-    expect(screen.queryByText("No results").textContent).toMatch(/no results/i);
+    expect(screen.queryByText("No results!").textContent).toMatch(
+      /no results!/i,
+    );
+  });
 
-    spiedFetch.mockRestore();
+  it("should render user by searching", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/", "/users"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    const response = await fetch(`${localhostURL}/login`, {
+      method: "POST",
+    });
+
+    await expect(response.json()).resolves.toEqual({
+      username: "preslaw",
+      password: "12345678B",
+    });
+
+    await user.type(screen.queryByLabelText("username"), "preslaw");
+
+    expect(screen.queryByLabelText("username").value).toEqual("preslaw");
+
+    await user.type(screen.queryByLabelText("password"), "12345678B");
+
+    expect(screen.queryByLabelText("password").value).toEqual("12345678B");
+
+    await user.click(screen.queryByRole("button", { name: "Login" }));
+
+    expect(screen.queryByAltText("loading spinner")).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByAltText("loading spinner"),
+    );
+
+    await user.click(screen.queryByText("Users"));
+
+    await user.type(screen.queryByLabelText("query"), "preslaw");
+
+    expect(screen.queryByLabelText("query").value).toEqual("preslaw");
+
+    // fireEvent.keyPress(screen.queryByLabelText("query"), {
+    //   key: "Enter",
+    //   code: "Enter",
+    //   charCode: 13,
+    // });
+
+    // screen.debug();
   });
 });
